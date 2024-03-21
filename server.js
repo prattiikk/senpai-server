c38e443ebcaadec4236752c57e556a9bb24e37c4
prattiikk <pvks5423@gmail.com>
2024-03-21 22:55:51 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 const authRoutes = require("./routes/auth.js");
 const commandRoutes = require("./routes/getCommands.js");
